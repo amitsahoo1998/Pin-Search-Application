eb5c999dff85bb610047cae80c4dad8c12728f3a
Amit Sahoo <amit.sahoo@bankify.money>
2023-08-16 18:06:07 +0530

    post office state bug fixed

--- app/src/main/java/com/neoapp/pinsearch/presenter/view/PinViewModel.kt
+++ app/src/main/java/com/neoapp/pinsearch/presenter/view/PinViewModel.kt
@@ -75,7 +75,7 @@ class PinViewModel @Inject constructor(
     fun setPostOffice(postOffice: String) {
         setState { state ->
             state.copy(
-                postOffice = postOffice.trim()
+                postOffice = postOffice
             )
         }
     }
